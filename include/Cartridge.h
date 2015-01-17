a047e03e8c1762e64b891147ae09d096d5524512
wornwinter <darrena092@gmail.com>
2015-01-17 13:36:40 +0000

    Modified header to make use of new struct.

--- include/Cartridge.h
+++ include/Cartridge.h
@@ -3,6 +3,7 @@
 
 #include <fstream>
 #include <iostream>
+#include "ROMHeader.h"
 
 class Cartridge
 {
@@ -19,12 +20,7 @@ public:
 
 private:
     bool romLoaded = false;
-
-    uint8_t m_MBCType = 0; //What MBC chip does the cart have?      @0x0147
-    uint8_t m_ROMSize = 0; //What size is our ROM?                  @0x0148
-    uint8_t m_RAMSize = 0; //How many ram Banks do we have?         @0x0149
-    uint8_t m_region  = 0; //0 = Japanese; 1 = Non-Japanese         @0x014A
-
+    s_ROMHeaderInfo ROMHeaderInfo;
     uint8_t rom0[0x8000]; //The actual ROM (32kB large, fixed ROM, no MBC :( )
 };
 
